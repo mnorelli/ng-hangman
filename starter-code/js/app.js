0ba9c92b92de5e61e81ceeb1606f437fd7ac2d3f
Michael Norelli <mnorelli360@gmail.com>
2016-07-06 13:43:31 -0700

    almost adds random word API

--- starter-code/js/app.js
+++ starter-code/js/app.js
@@ -5,13 +5,28 @@ angular
   .module("hangmanApp", [])
   .controller('hangmanCtrl', hangmanCtrl);
 
-hangmanCtrl.$inject = ["$scope"];
-function hangmanCtrl($scope){
-    $scope.hangman = new HangmanGame('elephant');
+hangmanCtrl.$inject = ["$scope","$http"];
+function hangmanCtrl($scope, $http){
 
-    $scope.guessLetter = function(guessed){
-      $scope.hangman.guess(guessed);
-      $scope.guessed = '';
+  var url = "http://randomword.setgetgo.com/get.php";
+  var url = url + "?callback=JSON_CALLBACK";
+
+  $http.jsonp(url)
+      .success(function(data){
+        $scope.wordToGet = data.Word;
+        console.log("wordToGet:"+$scope.wordToGet)
+      })
+      .error(function(data, status, headers, config){
+        alert("error:"+data)
+      });
+
+  console.log("wordToGet outside:"+$scope.wordToGet)
+  
+  $scope.hangman = new HangmanGame($scope.wordToGet);
+
+  $scope.guessLetter = function(guessed){
+    $scope.hangman.guess(guessed);
+    $scope.guessed = '';
 
     }
 
